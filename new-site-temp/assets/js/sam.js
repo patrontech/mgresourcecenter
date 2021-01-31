b6cb43cfa58108437984f0ebabf105a1dd79ff02
Sam Smith <smlksmth@gmail.com>
2021-01-31 18:51:25 -0500

    added the event contractor agreement

---
+++ new-site-temp/assets/js/sam.js
@@ -0,0 +1,12 @@
+$(function(){
+  $("#header").load("header.html");
+});
+
+$(function(){
+  $("#footer").load("footer.html");
+});
+
+
+$(function(){
+  $("#go-to-top").load("go-to-top.html");
+});
